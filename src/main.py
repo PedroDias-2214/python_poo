cb7a1d7c80dc0c75bcfc9411f7451fe6177388d6
PedroDias-2214 <pedrodgs014@gmail.com>
2024-08-19 20:43:29 -0300

    Simulacao de dois carros

--- src/main.py
+++ src/main.py
@@ -1,44 +1,57 @@
 from frota import *
 
+def operar_carro(carro: Carro):
+    print('1- Ligar motor')
+    print('2- Desligar motor')
+    print('3- Acelerar')
+
+    op = 0
+    while op not in (1, 2, 3):
+        op = int(input("Digite as opcoes[1-3]: "))
+
+    if op == 1:
+        carro.ligar()
+    elif op == 2:
+        carro.desligar()
+    elif op == 3:
+        v = float(input("Informe a velocidade: "))
+        t = float(input("Informe o tempo: "))
+        carro.acelerar(v, t)
+    print('Infos atuais do carro')
+    print(carro)
+
 if __name__ == "__main__":
-    print('Cadastre um carro')
+    print('Cadastre o carro 1')
     nm_modelo = input('Digite o modelo: ')
     nm_marca = input('Digite a marca: ')
     nm_cor = input('Digite a cor: ')
 
-    kms = float(input('Digite com quantos Kms: '))
+    carro1 = Carro(nm_modelo, nm_marca, nm_cor, 0, motor = False)
 
-    carro1 = Carro(nm_modelo, nm_marca, nm_cor, kms, motor = True)
+    print('Cadastre o carro 2')
+    nm_modelo = input('Digite o modelo: ')
+    nm_marca = input('Digite a marca: ')
+    nm_cor = input('Digite a cor: ')
+
+    carro2 = Carro(nm_modelo, nm_marca, nm_cor, 0, motor = False)
 
     '''
     Controlando o carro até ele atingir 10000 Km
     '''
-    while carro1.odometro < 10000:
+    while carro1.odometro < 600 and carro2.odometro < 600:
         try:
-            print('1- Ligar motor')
-            print('2- Desligar motor')
-            print('3- Acelerar')
+            numero_carro = 0
+            while numero_carro not in [1, 2]:
+                numero_carro = int (input('Informe o carro [1, 2]:'))
+            if numero_carro == 1: operar_carro (carro1)
+            elif numero_carro == 2: operar_carro (carro2)
 
-            op = 0
-            while op not in (1,2,3):
-                op = int(input("Digite as opcoes[1-3]: "))
-
-            if op == 1:
-                carro1.ligar()
-            elif op == 2:
-                carro1.desligar()
-            elif op == 3:
-                v = float(input("Informe a velocidade: "))
-                t = float(input("Informe o tempo: "))
-                carro1.acelerar(v, t)
-
-            print('Infos atuais do carro')
-            print(carro1)
         except Exception as e:
             print("Erro!")
             print(e)
+    if carro1.motor_on: carro1.desligar()
+    if carro2.motor_on: carro2.desligar()
 
-    carro1.desligar()
-    print(carro1)
-    print('Parar para trocar óleo!!!')
+    if carro1.odometro >= 600: print ('O carro 1 venceu a corrida')
+    else: print ('O carro 2 venceu a corrida')
 
